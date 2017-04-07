4cc9233e9abbfd18df33b35594ceec420be7b9fd
SimMue <simonmueller1@hotmail.com>
2017-04-07 17:20:23 +0200

    style game-component and add css

--- tick-tack-toe/src/app/app.module.ts
+++ tick-tack-toe/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu-component';
+import { GameComponent } from './game/game-component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    GameComponent,
   ],
   imports: [
     BrowserModule,
