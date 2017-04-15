6a9b45c5bc3834f449c9e8a97b8b2388457869cd
Simon Müller <simonmueller1@hotmail.com>
2017-04-15 16:36:54 +0200

    add key input events

--- snake-game/src/app/game.component.ts
+++ snake-game/src/app/game.component.ts
@@ -1,20 +1,28 @@
 import { Component, DoCheck } from '@angular/core';
 import { SettingsService } from './settings.service';
+import { CONTROLS, DIRECTIONS } from './game.constants';
 
 @Component({
     selector: 'main-game',
     templateUrl: './game.component.html',
     styleUrls: ['./game.component.css'],
+    host: {
+		'(document:keydown)': 'handleKeyboardEvents($event)'
+	},
 }
 )
 
 export class GameComponent implements DoCheck {
-    board: number[][];
+    board: boolean[][];    
     boardFieldSize : string;
-    boardFieldMargin : string;   
-    
+    boardFieldMargin : string;
+    direction : number[];
+    position : number[];
+    cancel : boolean = false;
 
-    constructor(private settingsService: SettingsService) {        
+    constructor(private settingsService: SettingsService) {
+        this.position = [0, 0];
+        this.direction = DIRECTIONS.RIGHT;
         this.initBoard();
         this.initBoardFieldCss(this.settingsService.getHeight());
     }
@@ -25,16 +33,66 @@ export class GameComponent implements DoCheck {
             var column = [];
 
             for (var r = 0; r < this.settingsService.getWidth(); r++) {
-                column.push(0);
+                column.push(false);
             }
 
             this.board.push(column);
         }
+        this.board[this.position[0]][this.position[1]] = true;
     }
 
     initBoardFieldCss(factor : number) {
-        this.boardFieldSize = 90 / factor + "%";
+        this.boardFieldSize = 75 / factor + "%";
         this.boardFieldMargin = 2 / factor + "%";
+        
+    }
+
+    startNewGame() {
+        this.move();
+    }
+
+    move() {
+        setTimeout(() => {
+                if (this.cancel === false){
+                    this.board[this.position[0]][this.position[1]] = false;
+                    this.validateNewPosition();
+                    this.board[this.position[0]][this.position[1]] = true;
+
+                    this.move();           
+                }
+                else
+                {
+                    this.cancel = false;
+                }
+        }, 500);
+    }
+
+    validateNewPosition() {
+        this.position[0] = this.position[0] + this.direction[0];
+        this.position[1] = this.position[1] + this.direction[1];
+        if (this.position[1] > this.board.length-1){
+             this.position[1] = 0;
+        }
+        else if (this.position[0] > this.board[0].length-1)
+        {   
+            this.position[0] = 0;
+        }
+    }
+
+    handleKeyboardEvents(e: KeyboardEvent) {
+		if (e.keyCode == CONTROLS.LEFT && this.direction !== DIRECTIONS.RIGHT) {
+			this.direction = DIRECTIONS.LEFT;
+		} else if (e.keyCode == CONTROLS.UP && this.direction !== DIRECTIONS.DOWN) {
+			this.direction = DIRECTIONS.UP;
+		} else if (e.keyCode == CONTROLS.RIGHT && this.direction !== DIRECTIONS.LEFT) {
+			this.direction = DIRECTIONS.RIGHT;
+		} else if (e.keyCode == CONTROLS.DOWN && this.direction !== DIRECTIONS.UP) {
+			this.direction = DIRECTIONS.DOWN;
+		}
+	}
+
+    cancelGame() {
+        this.cancel = true;
     }
 
     ngDoCheck(): void {
---
+++ snake-game/src/app/game.constants.ts
@@ -0,0 +1,13 @@
+export const CONTROLS = {
+	LEFT: 37,
+	UP: 38,
+	RIGHT: 39,
+	DOWN: 40
+};
+
+export const DIRECTIONS = {
+    LEFT: [0, -1],
+    UP: [-1, 0],
+    RIGHT: [0, 1],
+    DOWN: [1, 0]
+}
